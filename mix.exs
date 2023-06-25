078fdfa0cdb03c8919fb74e3795af7d4c5752650
Mark Sadegi <mark.sadegi@gmail.com>
2023-06-25 13:30:31 -0700

    Add workflow file for Github Actions

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule InfluenceAvenue.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:nimble_csv, "~> 1.1"}
+      {:nimble_csv, "~> 1.1"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
